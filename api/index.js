e4c8697cca2257f392f0e01601f4a3be0af197f1
Guilherme Loureiro <guilherme631999@gmail.com>
2024-11-26 23:02:50 +0000

    trying to make this dynamic

--- api/index.js
+++ api/index.js
@@ -1,4 +1,26 @@
-// api/index.js
-export default async function handler(req, res) {
-    res.status(200).json({ message: "Hello from Node.js on Vercel!" });
-}
+const path = require('path');
+const ejs = require('ejs');
+const fs = require('fs');
+
+module.exports = (req, res) => {
+  // Read the EJS template from the views folder
+  const filePath = path.resolve('views', 'index.ejs');
+
+  // Dynamic data
+  const dynamicData = {
+    title: "Geo Caching",
+    description: "Welcome to the world of Geo Caching (purely online) <br/> For all the lovers of Riddles, Map searching and much more",
+    howToPlay: "Riddles and Questions will appear on this side and you need to click on the right place on the map",
+    victoryPrize: "Well ... Knowledge ...",
+    startText: "Lets start strong select in the map the coolest country (I am portuguese)"
+  };
+
+  // Read the EJS file and render it
+  ejs.renderFile(filePath, dynamicData, {}, (err, html) => {
+    if (err) {
+      res.status(500).send('Error rendering the page.');
+      return;
+    }
+    res.send(html);
+  });
+};
