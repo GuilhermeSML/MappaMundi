00dd9fb9270de95aa686de0fa6725e465000514a
Guilherme Loureiro <guilherme631999@gmail.com>
2024-11-26 21:03:41 +0000

    Init commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const Map = () => {
+  return (
+    <MapContainer center={[0, 0]} zoom={2} style={{ height: '100vh', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>A sample marker in London.</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
